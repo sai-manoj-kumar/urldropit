3ad861d9d1602f15c8de7bec1409d8e9f0d3213a
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-11-23 00:48:05 +0530

    Adds auto-datetime, filename(if exist) to yesnofeedback

--- model.py
+++ model.py
@@ -3,4 +3,6 @@ from google.appengine.ext import db
 
 class YesNoFeedback(db.Model):
     url = db.StringProperty(required=True)
+    filename = db.StringProperty()
     success = db.BooleanProperty(required=True)
+    timestamp = db.DateTimeProperty(auto_now_add=True)
--- yesnofeedback.py
+++ yesnofeedback.py
@@ -7,13 +7,13 @@ import model
 class YesNoFeedback(handler.Handler):
     def post(self):
         url = json.loads(self.request.body).get('url', False).strip()
+        filename = json.loads(self.request.body).get('filename', None)
         status = json.loads(self.request.body).get('success', None)
-        logging.info(self.request.headers)
         if status:
             logging.info('Success: %s' % url)
         else:
             logging.error('Failed: %s' % url)
         self.response.write('Response Taken')
         if url not in [None, ''] or True:
-            yesno = model.YesNoFeedback(url=url, success=status)
+            yesno = model.YesNoFeedback(url=url, filename=filename, success=status)
             yesno.put()
