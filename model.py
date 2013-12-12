e21a84c34c52ede98b32edb070561697d476436b
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-12-12 20:52:52 +0530

    Fixes Issue#1, Allows url longer than 500 chars

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from google.appengine.ext import db
 
 
 class YesNoFeedback(db.Model):
-    url = db.StringProperty(required=True)
+    url = db.TextProperty(required=True)
     filename = db.StringProperty()
     success = db.BooleanProperty(required=True)
     timestamp = db.DateTimeProperty(auto_now_add=True)
