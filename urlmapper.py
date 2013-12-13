70317321a87db9a474ac127aeccd5d477074935b
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-12-13 21:32:18 +0530

    Removes overhead of page reload, shows progress

--- urlmapper.py
+++ urlmapper.py
@@ -2,14 +2,12 @@ import webapp2
 import flattr
 import mainpage
 import credits
-import download
 import yesnofeedback
 
 
 app = webapp2.WSGIApplication(
     [
         ('/', mainpage.MainPage),
-        ('/download', download.DownloadPage),
         ('/yesnofeedback', yesnofeedback.YesNoFeedback),
         ('/flattr', flattr.Flattr),
         ('/credits', credits.Credits)
