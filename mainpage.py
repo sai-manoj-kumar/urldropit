6370d4f336fff416ab77fa0a797a15dee3fe5a32
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-12-14 12:59:41 +0530

    Inserts photo and roles in credits
    Renames sb_form.jinja2 to mainpage.jinja2

--- mainpage.py
+++ mainpage.py
@@ -5,6 +5,6 @@ class MainPage(handler.Handler):
     def get(self):
         error = self.request.get('error')
         if error is not None:
-            self.render('sb_form.jinja2', active='home', error=error)
+            self.render('mainpage.jinja2', active='home', error=error)
         else:
-            self.render('sb_form.jinja2', active='home')
+            self.render('mainpage.jinja2', active='home')
